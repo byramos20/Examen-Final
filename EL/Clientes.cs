b81528888e4588d6710405ecf45aba64d4568867
byramos20 <ramos102013@gmail.com>
2023-02-25 12:44:30 -0600

    Agregar archivos de proyecto.

---
+++ EL/Clientes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema; 
+
+namespace EL
+{
+    [Table("Clientes")]
+    public class Clientes
+    {
+        [Key]
+        public int IdCliente { get; set; }
+
+        [Required]
+        public String NombreCompleto { get; set; }
+
+        [Required]
+        public String Identificacion { get; set; }
+
+        [Required]
+        public String Celular { get; set; }
+
+        [Required]
+        public String Correo { get; set; }
+
+        //pistas de auditoria 
+
+        public int IdUsuarioRegistro { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public int IdUsuarioActualiza { get; set; }
+        public DateTime FechaActualizacion { get; set; }
+        public bool Activo { get; set; }
+    }
+}
